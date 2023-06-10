aabcc96411f5823765b6381a77007a15e1836e71
Henrique <henriquebzn1@gmail.com>
2023-06-09 22:55:02 -0300

    Animated rest of the header

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
-const timeline = gsap.timeline();
+const timeline = gsap.timeline({
+  defaults: { duration: 0.5, ease: "power1", autoAlpha: 0 },
+});
+
+const iconAnimation = {
+  scale: 0,
+  delay: "-0.3",
+};
 
 timeline
   .from("h2", {
-    duration: 0.5,
     x: "-10%",
-    autoAlpha: 0,
     opacity: 0,
-    ease: "power1",
     delay: 1.5,
   })
   .from("h1", {
-    duration: 0.5,
     x: "10%",
-    autoAlpha: 0,
     opacity: 0,
-    ease: "power1",
-  });
+  })
+  .from(".icon:nth-child(1)", iconAnimation)
+  .from(".icon:nth-child(2)", iconAnimation)
+  .from(".icon:nth-child(3)", iconAnimation)
+  .from(["main", "footer"], { y: "10%", autoAlpha: 0 });
